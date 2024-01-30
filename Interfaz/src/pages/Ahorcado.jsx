6ecab1906ed8024bc45a390d6f6cf87f041c8f58
PaoMarroquin <pmarroquinpi@unsa.edu.pe>
2024-01-29 19:41:32 -0500

    ultimos cambios al juego ahorcado

--- Interfaz/src/pages/Ahorcado.jsx
+++ Interfaz/src/pages/Ahorcado.jsx
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import imagen1 from './img/ahorcado1.webp';
+import imagen2 from './img/ahorcado2.webp';
+import imagen3 from './img/ahorcado3.webp';
+import imagen4 from './img/ahorcado4.webp';
+import imagen5 from './img/ahorcado5.webp';
+import imagen6 from './img/ahorcado6.webp';
 
 const Ahorcado = () => {
   const { juego } = useParams();
-  const [palabraOculta, setPalabraOculta] = useState("");
+  const [palabraOculta, setPalabraOculta] = useState([]);
   const [letrasAdivinadas, setLetrasAdivinadas] = useState([]);
   const [letrasIncorrectas, setLetrasIncorrectas] = useState([]);
   const [vidas, setVidas] = useState(6);
@@ -18,7 +24,8 @@ const Ahorcado = () => {
 
   const reiniciarJuego = () => {
     const palabras = ["perro", "gato", "casa", "árbol", "coche"];
-    setPalabraOculta(palabras[Math.floor(Math.random() * palabras.length)]);
+    const palabraSeleccionada = palabras[Math.floor(Math.random() * palabras.length)];
+    setPalabraOculta(palabraSeleccionada.split(""));
     setLetrasAdivinadas([]);
     setLetrasIncorrectas([]);
     setVidas(6);
@@ -43,7 +50,7 @@ const Ahorcado = () => {
     }
 
     setInputLetra("");
-    
+
     // Verificar si todas las letras han sido adivinadas
     if (!getPalabraOculta().includes("_")) {
       setEstadoJuego("ganado");
@@ -56,56 +63,65 @@ const Ahorcado = () => {
 
   const getPalabraOculta = () => {
     return palabraOculta
-      .split("")
-      .map(letra => (letrasAdivinadas.includes(letra) ? letra : "_"))
+      .map((letra) => (letrasAdivinadas.includes(letra) ? letra : "_"))
       .join(" ");
   };
-  //cambios
-  const dibujoAhorcado = () => {
-    const dibujo = [
-      "  ________",
-      " |        |",
-      " |        " + (vidas < 6 ? "O" : ""),
-      " |       " + (vidas < 5 ? "/" : "") + (vidas < 4 ? "|" : "") + (vidas < 3 ? "\\" : ""),
-      " |       " + (vidas < 2 ? "/" : "") + " " + (vidas < 1 ? "\\" : ""),
-      " |",
-      " |___"
-    ];
 
-    return dibujo.map((line, index) => <div key={index}>{line}</div>);
-  };
+  const LetraCuadro = ({ letra }) => (
+    <div className="border border-gray-300 rounded-md p-2 m-1">
+      {letrasAdivinadas.includes(letra) ? letra : "_"}
+    </div>
+  );
+
+  const imagenes = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6];
+
+  const [imagenPath, setImagenPath] = useState(imagen1); // Comienza con la imagen inicial
+  useEffect(() => {
+    const errors = 6 - vidas; // Calcular errores en función de las vidas restantes
+    const newImagenPath = imagenes[errors]; // Obtener la ruta de la imagen correspondiente
+    setImagenPath(newImagenPath);
+  }, [vidas]);
 
   return (
-    <div>
-      <div>
-        {dibujoAhorcado()}
+    <div className="flex flex-col items-center space-y-4  ">
+       <div className="flex items-center justify-center mt-4">
+      <img src={imagenPath} alt="Ahorcado" style={{ width: '90px', height: '90px' }}/>
       </div>
-      <div>
-        <p>Palabra oculta: {getPalabraOculta()}</p>
-        <p>Letras adivinadas: {letrasAdivinadas.join(", ")}</p>
-        <p>Letras incorrectas: {letrasIncorrectas.join(", ")}</p>
-        {estadoJuego === "jugando" && (
-          <>
-            <input
-              type="text"
-              id="letra"
-              placeholder="Introduce una letra"
-              maxLength="1"
-              value={inputLetra}
-              onChange={(e) => setInputLetra(e.target.value)}
-            />
-            <button onClick={comprobarLetra}>Comprobar</button>
-          </>
-        )}
-        {(estadoJuego === "perdido" || estadoJuego === "ganado") && (
-          <button onClick={reiniciarJuego}>Reiniciar</button>
-        )}
-      </div>
-      <div>
+      <div className="flex items-center">
         <p>Vidas restantes: {vidas}</p>
         {[...Array(vidas)].map((_, index) => (
           <FontAwesomeIcon key={index} icon={faHeart} style={{ color: "red", margin: "2px" }} />
         ))}
+      </div>
+      <div className="overflow-auto flex items-center h-full">
+        {palabraOculta.map((letra, index) => (
+          <LetraCuadro key={index} letra={letra} />
+        ))}
+      </div>
+      {estadoJuego === "jugando" && (
+        <div className="flex items-center space-x-2 ">
+          <input
+            type="text"
+            id="letra"
+            placeholder="Introduce una letra"
+            maxLength="1"
+            value={inputLetra}
+            onChange={(e) => setInputLetra(e.target.value)}
+            className="border border-gray-300 p-2 rounded-md"
+          />
+          <button onClick={comprobarLetra} className="bg-gray-300 p-2 rounded-full hover:bg-gray-600 hover:text-white">
+            Comprobar
+          </button>
+        </div>
+      )}
+      {(estadoJuego === "perdido" || estadoJuego === "ganado") && (
+        <button onClick={reiniciarJuego} className="bg-gray-300 p-2 rounded-full hover:bg-gray-600 hover:text-white">
+          Reiniciar
+        </button>
+      )}
+      <div>
+        <p>Letras adivinadas: {letrasAdivinadas.join(", ")}</p>
+        <p>Letras incorrectas: {letrasIncorrectas.join(", ")}</p>
       </div>
       {haTerminado() && (
         <div>
